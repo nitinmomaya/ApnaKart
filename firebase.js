828041fe35c8856050fe7cbf19e2734c6d4b9fb9
Nitin Momaya <nitinmomaya5@gmail.com>
2023-03-08 00:03:21 +0530

    firebase file added

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxZ2uzlHpOxSpUjoeWo0jDA8pEk4S_BgE",
+  authDomain: "apnakart-nitinmomaya.firebaseapp.com",
+  projectId: "apnakart-nitinmomaya",
+  storageBucket: "apnakart-nitinmomaya.appspot.com",
+  messagingSenderId: "528826948292",
+  appId: "1:528826948292:web:92ba3adcc2817284ee4791",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
